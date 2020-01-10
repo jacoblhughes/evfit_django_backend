6846e4b339b2fd753618f17a2fc275262e845621
Jacob Hughes <hughes.jacobl@gmail.com>
2020-01-10 08:48:30 -0800

    First commit of app for heroku deployment evidentfitness.com/evfitportal

---
+++ passenger_wsgi.py
@@ -0,0 +1,10 @@
+import imp
+import os
+import sys
+# set variables
+sys.path.append(os.getcwd())
+os.environ['DJANGO_SETTINGS_MODULE'] = 'efportal.settings'
+    #setup django application
+import django.core.handlers.wsgi
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
