3580add592134891c41c9f1481af2ab0c9b093c0
Jacob Hughes <hughes.jacobl@gmail.com>
2020-01-11 15:05:47 -0800

    hopefully, I finally figured out the CSS issue

--- efportal/wsgi.py
+++ efportal/wsgi.py
@@ -12,14 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'efportal.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root= STATIC_ROOT )
+application = WhiteNoise(application)
