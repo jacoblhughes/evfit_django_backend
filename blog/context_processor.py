0cb8896b64398f2cfae9bdd5de5384daec0cb193
Jacob Hughes <hughes.jacobl@gmail.com>
2020-02-02 09:17:57 -0800

    got it working?

--- blog/context_processor.py
+++ blog/context_processor.py
@@ -1,5 +1,5 @@
 from blog.models import BlogPost
 
 def recent_five(request):
-    context = BlogPost.objects.filter(status=1).order_by('-created_on')
+    context = BlogPost.objects.filter(status=1).order_by('-created_on')[:5]
     return {'recent_five': context}
