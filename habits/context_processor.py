efed9070bc723bca9784793df6fe4a5dc853ffb3
Jacob Hughes <hughes.jacobl@gmail.com>
2020-02-02 09:14:58 -0800

    got it working?

--- habits/context_processor.py
+++ habits/context_processor.py
@@ -1,8 +1,11 @@
 from habits.models import Habit, HabitMember
-
 def user_habits_context(request):
-    context = HabitMember.objects.filter(user = request.user)
-    return {'user_habits_context': context}
+    if request.user.is_authenticated:
+        context = HabitMember.objects.filter(user = request.user)
+        return {'user_habits_context': context}
+    elif request.user.is_anonymous:
+        context = HabitMember.objects.filter(user = 1)
+        return {'user_habits_context': context}
 
 def all_habits_context(request):
     context = Habit.objects.all()
