c61f0a874457fc3c9eec1d49af8a2f5c94e3d9de
Jacob Hughes <hughes.jacobl@gmail.com>
2020-03-24 16:54:58 -0700

    working on api

--- accounts/context_processor.py
+++ accounts/context_processor.py
@@ -1,5 +1,5 @@
 from accounts.models import Profile
 
 def profile_information(request):
-    context = Profile.objects.all().order_by('-id')
+    context = Profile.objects.all()
     return {'profile_information': context}
