fd973287d1c5841e3457a260e6de7e063f7ce5f0
Jacob Hughes <hughes.jacobl@gmail.com>
2021-01-15 20:41:38 -0800

    order and status of posts

--- blog/api/views.py
+++ blog/api/views.py
@@ -6,5 +6,5 @@ from rest_framework import permissions
 
 class EFBlogViewset(viewsets.ModelViewSet):
 
-    queryset = models.BlogPost.objects.all().order_by('created_on')[:5]
+    queryset = models.BlogPost.objects.filter(status=1).order_by('-created_on')[:5]
     serializer_class = serializers.EFBlogSerializer
